b215a53b2f8beb52e371712e6419b0045eebf2a0
David Barksdale <amatus@amatus.name>
2016-06-06 21:49:17 -0500

    Add code to drive the display

--- rfidbadge.ino
+++ rfidbadge.ino
@@ -1,11 +1,18 @@
 #include <Wire.h>
 #include "CLRC663.h"
+#include "ST7565.h"
 
+#define LED  (8)
 Clrc663 rc663 = Clrc663();
+ST7565 lcd = ST7565(11, 13, 9, 12, 10);
 
 void setup()
 {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
+  lcd.begin(0x18);
+  lcd.drawstring(0, 0, "Scanning...");
+  lcd.display();
   Wire.begin();
   rc663.iso_14443A_init();
 }
@@ -22,6 +29,19 @@ void loop()
   Serial.print(atqa[0], HEX);
   Serial.print(", ");
   Serial.println(atqa[1], HEX);
+  if (rc == 2) {
+    digitalWrite(LED, HIGH);
+    char buf[2];
+    itoa(atqa[0], buf, 16);
+    lcd.drawstring(0, 1, buf);
+    itoa(atqa[1], buf, 16);
+    lcd.drawstring(0, 2, buf);
+  } else {
+    digitalWrite(LED, LOW);
+    lcd.drawstring(0, 1, "  ");
+    lcd.drawstring(0, 2, "  ");
+  }
+  lcd.display();
   delay(1000);
 }
 
