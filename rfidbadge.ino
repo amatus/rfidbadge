9f5896238dc7837cb8aeee2c8ba1ae8683ebf2ed
David Barksdale <amatus@amatus.name>
2016-07-13 20:00:41 -0500

    Use EEPROM to store scan period

--- rfidbadge.ino
+++ rfidbadge.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Wire.h>
 #include "CLRC663.h"
 #include "ST7565.h"
@@ -5,8 +6,21 @@
 #define LED    (8)
 #define MENU   (3)
 #define SELECT (4)
+#define EEPROM_MAGIC  (0xDB)
+#define DEFAULT_SCAN_DECISECONDS  (10)
 Clrc663 rc663 = Clrc663();
 ST7565 lcd = ST7565(11, 13, 9, 12, 10);
+unsigned long scan_millis; // time between scans
+unsigned long last_scan;   // last time scanned
+boolean scan; // is scanning enabled
+unsigned long led_millis = 5 * 1000;  // time to leave the led on
+unsigned long last_led;    // last time the led was turned on
+
+void trigger_led()
+{
+  digitalWrite(LED, HIGH);
+  last_led = millis();
+}
 
 void setup()
 {
@@ -16,14 +30,26 @@ void setup()
   Serial.begin(9600);
   lcd.begin(0x08);
   Wire.begin();
+  byte magic = EEPROM.read(0);
+  if (EEPROM_MAGIC != magic) {
+    EEPROM.write(0, EEPROM_MAGIC);
+    EEPROM.write(1, DEFAULT_SCAN_DECISECONDS);
+    scan_millis = DEFAULT_SCAN_DECISECONDS * 100;
+  } else {
+    scan_millis = EEPROM.read(1) * 100;
+  }
+  last_scan = millis() - scan_millis;
+  scan = 0 != scan_millis;
+  lcd.clear();
+  lcd.drawstring(0, 0, scan ? (char *)"Scanning..." : (char *)"Not scanning!");
+  lcd.display();
+  trigger_led();
 }
 
-void loop()
+void do_scan()
 {
-  lcd.clear();
-  lcd.drawstring(0, 0, "Scanning...");
-  rc663.iso_14443A_init();
   int rc;
+  rc663.iso_14443A_init();
   {
     Serial.println("Requesting ATQA");
     uint8_t atqa[2];
@@ -37,9 +63,8 @@ void loop()
     Serial.println(atqa[1], HEX);
   }
   if (rc == 2) {
-    digitalWrite(LED, HIGH);
+    trigger_led();
   } else {
-    digitalWrite(LED, LOW);
     goto out;
   }
   {
@@ -66,7 +91,17 @@ void loop()
 out:
   rc663.transmit_enable(false);
   lcd.display();
-  delay(1000);
+}
+
+void loop()
+{
+  if (scan && millis() - last_scan >= scan_millis) {
+    last_scan = millis();
+    do_scan();
+  }
+  if (digitalRead(LED) == HIGH && millis() - last_led >= led_millis) {
+    digitalWrite(LED, LOW);
+  }
 }
 
 /* vim: set expandtab ts=2 sw=2: */
